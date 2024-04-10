1b49206f0bc6ee955c47b3f8c7b4ae307803874a
zhuwq <zhuwq0@gmail.com>
2024-04-09 23:35:23 -0700

    re-organized files

--- 2D/tests/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-setup(name='eik2d_cpp',
-      ext_modules=[cpp_extension.CppExtension('eik2d_cpp', ['Eikonal2Dt.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-setup(name='eik2d_cpp',
-      ext_modules=[cpp_extension.CppExtension('eik2d_cpp', ['Eikonal2Dt.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
+setup(
+    name="adtomo",
+    version="0.1.0",
+    packages=find_packages(),
+    ext_modules=[
+        CppExtension(
+            name="eik2d_cpp",
+            sources=[],
+            extra_compile_args=[],
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension.with_options(no_cuda=True)},  # Disables CUDA, compile only for CPU
+    install_requires=[
+        "torch",
+    ],
+    python_requires=">=3.6",
+)
